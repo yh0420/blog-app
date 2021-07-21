86f9fc5d5598d74032272bc7191d43075b08796a
yuta higa <yh19950420@gmail.com>
2021-07-21 19:43:35 +0900

    formを部分プレートに変更した 投稿の日付を取得して変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
+  
   def index
     @articles = Article.all
   end
   def show
-    @article = Article.find(params[:id])
+    
   end
   def new
     @article = Article.new
@@ -20,11 +22,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-   @article = Article.find(params[:id])
+   
   end
 
   def update
-    @article = Article.find(params[:id])
+    
     if @article .update(article_params)
       redirect_to article_path(@article),notice: '更新できました'
     else
@@ -44,5 +46,9 @@ end
   private
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
