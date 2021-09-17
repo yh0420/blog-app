b776e3e5a1c71eb2d1cc435664f4f880fff14b30
yuta higa <yh19950420@gmail.com>
2021-09-17 17:21:43 +0900

    いいね機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,10 +22,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :favorite_articles, through: :likes, source: :article
   has_one :profile, dependent: :destroy
+
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
+  
   def has_written?(article)
     articles.exists?(id: article.id)
+  end
+  def has_liked?(article)
+    likes.exists?(article_id: article.id)
   end
   def display_name
     profile&.nickname || self.email.split('@').first
